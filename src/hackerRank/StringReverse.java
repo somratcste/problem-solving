c32b367e1e4665aaf12d3bbb9cab47d3876a8f41
somratcste <nazmul.avanto@gmail.com>
2020-05-31 14:29:41 +0200

    string reverse pblm

---
+++ src/hackerRank/StringReverse.java
@@ -0,0 +1,19 @@
+package hackerRank;
+
+import java.io.*;
+import java.util.*;
+
+public class StringReverse {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String r = new StringBuilder(A).reverse().toString();
+        if (A.equals(r)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+}
