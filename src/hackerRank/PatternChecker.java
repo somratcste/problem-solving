c0b79b123fdc48e65aa7099ba0be081a43197ffb
somratcste <nazmul.avanto@gmail.com>
2020-06-01 17:53:51 +0200

    regex pattern

---
+++ src/hackerRank/PatternChecker.java
@@ -0,0 +1,21 @@
+package hackerRank;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class PatternChecker {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases > 0){
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+}
